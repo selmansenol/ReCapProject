55fb5376ae4008b7723b934bc9e16f55671a5865
Selman <69358460+selmansenol@users.noreply.github.com>
2021-02-11 02:12:58 +0300

    Add files via upload

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,31 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : IEfEntityRepositoryBase<Car, ReCapProjectDBContext>, ICarDal
+    {
+        public List<CarDetailsDto> GetCarDetails()
+        {
+            using (ReCapProjectDBContext context = new ReCapProjectDBContext())
+            {
+                var resultList = from c in context.Cars
+                                 join b in context.Brands on c.BrandID equals b.BrandID
+                                 join cl in context.Colors on c.ColorID equals cl.ColorID
+                                 select new CarDetailsDto { CarID = c.CarID, BrandName = b.BrandName, ColorName = cl.ColorName, DailyPrice = c.DailyPrice };
+
+               return resultList.ToList();
+
+            } 
+        }
+
+    }
+}
